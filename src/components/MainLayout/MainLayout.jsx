afc7ade09d85ac5a2376f5570f18653f2956c003
An Bin <quocthienhong@gmail.com>
2024-01-07 23:07:12 +0700

    push footer, mainLayout, scrollToTop

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import mainLayout from "./mainLayout.module.scss";
+import { TitleFunction } from "utils/TitleFunction";
+
+export default function MainLayout() {
+  TitleFunction("Movie");
+
+  return (
+    <Layout>
+      <Layout.Header className={mainLayout.mainHeader}>
+        <Header />
+      </Layout.Header>
+
+      <Layout.Content>
+        <Outlet />
+      </Layout.Content>
+
+      <Layout.Footer className={mainLayout.mainFooter}>
+        <Footer />
+      </Layout.Footer>
+    </Layout>
+  );
+}
