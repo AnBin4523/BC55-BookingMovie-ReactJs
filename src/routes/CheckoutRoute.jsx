9da43626178683fec6e3ceac2015991c7679b610
An Bin <quocthienhong@gmail.com>
2024-01-07 23:29:23 +0700

    push admin layout and auth  layout

---
+++ src/routes/CheckoutRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import swal from "sweetalert";
+
+export default function CheckoutRoute({ children }) {
+  const { user } = useSelector((state) => state.auth);
+
+  if (!user) {
+    swal("Vui lòng đăng nhập để đặt vé!", "You clicked the button!", "warning");
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
